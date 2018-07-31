a76f29861ad20f7d6542fa324fed8c61f7a87ac6
priya <swarnapriyasahu@gmail.com>
2018-07-31 20:13:37 +0530

    scrollfragment

---
+++ app/src/main/java/com/priya/scrollfragmentexample/ScrollFragmentActivity.java
@@ -0,0 +1,13 @@
+package com.priya.scrollfragmentexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScrollFragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_fragment);
+    }
+}
---
+++ app/src/main/java/com/priya/scrollfragmentexample/fragment/ScrollFragment.java
@@ -0,0 +1,22 @@
+package com.priya.scrollfragmentexample.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.priya.scrollfragmentexample.R;
+
+public class ScrollFragment extends Fragment
+{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view = inflater.inflate(R.layout.fragment,container,false);
+        return view;
+    }
+}
